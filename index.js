baf737b12196427911d7021868f99b7a1a9778a8
Alexey Spiridonov <ZayacZloy@yandex.ru>
2022-10-07 01:12:22 +0300

    feat: animation, title

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const form = document.querySelector('form');
 const volumeInput = document.getElementById('volume');
 const procInput = document.getElementById('proc');
-const title = document.querySelector('.form__title');
+const formTitle = document.querySelector('.form__title');
+
+function valuesSetter() {
+  const values = ((volumeInput.value/100*procInput.value) * 2.5);
+  formTitle.textContent = values;
+  formTitle.classList.add('form__title_type_visible');
+  volumeInput.value = '';
+  procInput.value = '';
+}
 
 function handleSubmit(e) {
   e.preventDefault();
-  const values = ((volumeInput.value/100*procInput.value) * 2.5);
-  title.textContent = values;
-  volumeInput.value = '';
-  procInput.value = '';
+  formTitle.textContent = '';
+  formTitle.classList.remove('form__title_type_visible');
+  setTimeout(valuesSetter, 200);
 }
 
 form.addEventListener('submit', handleSubmit);
